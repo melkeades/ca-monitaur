00976bd56d4eede387003df9db557d530bc2cbc7
melkeades <118681335+melkeades@users.noreply.github.com>
2024-01-20 01:15:29 +0300

    init

---
+++ main.js
@@ -0,0 +1,11 @@
+import Splide from '@splidejs/splide'
+import { AutoScroll } from '@splidejs/splide-extension-auto-scroll'
+import { Intersection } from '@splidejs/splide-extension-intersection'
+import '@splidejs/splide/css'
+import './style.styl'
+
+const sel = (e) => document.querySelector(e)
+const selAll = (e) => document.querySelectorAll(e)
+const l = (e) => console.log(e)
+
+l('qwe')
